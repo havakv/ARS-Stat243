2bf99bdbf544286d362254ad43cd088226cb4381
wangyang <wangyang@berkeley.edu>
2013-11-22 09:03:38 +0800

    primary function created by Yang

---
+++ adaptive_rejection_sampling_main.R
@@ -0,0 +1,40 @@
+## STAT 243 Final Project: adaptive rejection sampling ##
+## Jinze Gu, Havard Kvamme, Reid Stevens and Yang Wang ##
+
+## This is the primary function that performs the sampling from a log-concave probability density function using the adaptive rejection sampling algorithm developed by Gilks and Wild (Appl. Statist. 41, 337, 1992) ##
+
+#f is the target density function, n is the number of required sample, test indicates whether to perform the test, k is the initial number of abscissaes
+
+ars <- function(f, n, test=TRUE, left_bound = -Inf, right_bound = Inf, k=3) {
+  #Generating the initial abscissaes x
+  x <- initial(left_bound, right_bound, k)
+  fx <- f(x)
+  fpx <- fp(f, x)
+  sample <- rep(NA, n)
+  count <- 0
+
+  while (count < n) {
+    #Make the lower_bound, upper bound functions
+    lower_bound <- make_lower_bound(x, fx, left_bound, right_bound)
+    upper_bound <- make_upper_bound(x, fx, fpx, left_bound, right_bound)
+    
+    #Draw samples from the upper bound function
+    cand <- sample_upper_bound(n - count, x, fx, fpx, left_bound, right_bound)
+    
+    #"update" records the first point in the sample that can not be accepted based on lowerbound
+    #"accepted" records the candidates drawn from upper_bound function that are accepted by only using the lowerbound until the "update" point
+    cand_filtered <- filter(cand, lower_bound, upper_bound)
+    accepted <- cand_filtered[1]
+    update <- cand_filtered[2]
+    
+    #Update the sample using cand_filtered
+    sample <- update_sample(sample, cand_filtered)
+    count <- length(na.omit(sample))
+    
+    #Update the abscissaes x
+    x <- sort(c(x, update))
+    fx <- sort(c(fx, f(update)))
+    fpx <- sort(c(fpx, fp(f, update)))
+  }
+  return (sample)
+}
