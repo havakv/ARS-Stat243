0c9e8e6f6cb758e0784660daf50a7a6c5ceaee6d
wangyang <wangyang@berkeley.edu>
2013-11-23 08:22:47 +0800

    fixed the log bug

--- adaptive_rejection_sampling_main.R
+++ adaptive_rejection_sampling_main.R
@@ -8,19 +8,19 @@
 ars <- function(f, n, test=TRUE, left_bound = -Inf, right_bound = Inf, k=3) {
   #Generating the initial abscissaes x
   x <- initial(left_bound, right_bound, k)
-  fx <- f(x)
-  fpx <- fp(f, x)
+  hx <- log(f(x))
+  hpx <- log(fp(f, x))
   sample <- rep(NA, n)
   count <- 0
-  z <- make_z(x, fx, fpx, left_bound, right_bound) 
+  z <- make_z(x, hx, hpx, left_bound, right_bound) 
 
   while (count < n) {
     #Make the lower_bound, upper bound functions
-    lower_bound <- make_lower_bound(x, fx, z, left_bound, right_bound)
-    upper_bound <- make_upper_bound(x, fx, fpx, z, left_bound, right_bound)
+    lower_bound <- make_lower_bound(x, hx, z, left_bound, right_bound)
+    upper_bound <- make_upper_bound(x, hx, hpx, z, left_bound, right_bound)
     
     #Draw samples from the upper bound function
-    cand <- sample_upper_bound(n - count, x, fx, fpx, z, left_bound, right_bound)
+    cand <- sample_upper_bound(n - count, x, hx, hpx, z, left_bound, right_bound)
     
     #"update" records the first point in the sample that can not be accepted based on lowerbound
     #"accepted" records the candidates drawn from upper_bound function that are accepted by only using the lowerbound until the "update" point
@@ -33,11 +33,11 @@ ars <- function(f, n, test=TRUE, left_bound = -Inf, right_bound = Inf, k=3) {
     count <- length(na.omit(sample))
     
     #Update the abscissaes x
-    update_absci <- update_x(x, fx, fpx, update)
+    update_absci <- update_x(x, hx, hpx, update)
     x <- update_absci[1]
-    fx <- update_absci[2]
-    fpx <- update_absci[3]
-    z <- make_z(x, fx, fpx, left_bound, right_bound)
+    hx <- update_absci[2]
+    hpx <- update_absci[3]
+    z <- make_z(x, hx, hpx, left_bound, right_bound)
   }
   return (sample)
 }
