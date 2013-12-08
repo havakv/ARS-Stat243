ba3a1fa5b4f96b99ac9a9ffa08c37e2eb7bc5538
Haavard Kvamme <haavard.kvamme@gmail.com>
2013-12-07 16:00:55 -0800

    Fixed bugs in lower and upper bound. Some numerical issues.

--- ARS.R
+++ ARS.R
@@ -135,21 +135,30 @@ make_upper_bound_old <- function(x, hx, hpx, z) {
 }
 
 #################################################################
-make_lower_bound <- function(x, hx, left_bound, right_bound) {
-  x <- c(left_bound, x, right_bound)
+make_lower_bound <- function(x, hx) {
+  min <- min(x)
+  max <- max(x)
+  left <- min - abs(min*.Machine$double.eps)
+  right <- max + abs(max*.Machine$double.eps)
+  x <- c(left, x, right)
   hx <- c(-Inf, hx, -Inf)
-  lower_bound <- approxfun(x,hx)
+  lower_bound <- approxfun(x, hx, rule = c(2,2))
   return(lower_bound)
 }
 
 make_upper_bound <- function(x, hx, hpx, z) {
-  upper_bound_eval_z <- function(xx) {
-    index <- rowSums(outer(xx, z, function(x1,x2) x1>x2))
+  upper_bound_eval_z <- function(z) {
+    index <- rowSums(outer(z, z, function(x1,x2) x1>x2))
     index[index==0] <- 1
     index[index==length(z)] <- length(z) - 1
-    ux <- hx[index] + (xx-x[index])*hpx[index]
+    ux <- hx[index] + (z-x[index])*hpx[index]
     return (ux)
   }
+  # Because of numerical issues
+  # Need to find a better way to do to this. Is x sorted? 
+  m <- length(z)
+  if (z[1] == -Inf) z[1] <- -1000*max(abs(x))
+  if (z[m] == Inf) z[m] <- 1000*max(abs(x))
   y <- upper_bound_eval_z(z)
   upper_bound <- approxfun(z,y)
   return(upper_bound)
--- test_make_lowerupper_bound.R
+++ test_make_lowerupper_bound.R
@@ -6,9 +6,12 @@ x <- seq(-10.5, 10.5, by=4)
 hx <- -x^2
 hpx <- -2*x
 z <- make_z(x,hx,hpx,-12.0, 12.0)
-lower <- make_lower_bound(x, hx, -12.0, 12.0)
+lower <- make_lower_bound(x, hx)
 upper <- make_upper_bound(x, hx, hpx, z)
+lower_old <- make_lower_bound_old(x, hx, -12.0, 12.0)
+upper_old <- make_upper_bound_old(x, hx, hpx, z)
 xx <- seq(-12, 12, by=0.2)
+
 plot(xx, -xx^2, type='l')
 lines(xx, lower(xx), col="red")
 lines(xx, upper(xx), col="blue")
@@ -18,8 +21,10 @@ x <- seq(-10.5, 10.5, by=4)
 hx <- -x^2
 hpx <- -2*x
 z <- make_z(x,hx,hpx,-Inf, Inf)
-lower <- make_lower_bound(x, hx, -Inf, Inf)
+lower <- make_lower_bound(x, hx)
 upper <- make_upper_bound(x, hx, hpx, z)
+lower_old <- make_lower_bound_old(x, hx, -Inf, Inf)
+upper_old <- make_upper_bound_old(x, hx, hpx, z)
 xx <- seq(-20, 20, by=0.2)
 plot(xx, -xx^2, type='l')
 lines(xx, lower(xx), col="red")
@@ -33,11 +38,11 @@ source("make_lowerupper_bound.R")
 x <- seq(-10.5, 10.5, by=4)
 hx <- -x^2
 hpx <- -2*x
-z <- make_z(x,hx,hpx,-12.0, 12.0)
-lower_old <- make_lower_bound_old(x, hx, -12.0, 12.0)
-lower <- make_lower_bound(x, hx, -12.0, 12.0)
+z <- make_z(x,hx,hpx,-Inf, Inf)
+lower_old <- make_lower_bound_old(x, hx, -Inf, Inf)
+lower <- make_lower_bound(x, hx)
 
-xx <- runif(n = 1e5, min = -12, max = 12)
+xx <- runif(n = 1e5, min = -20, max = 20)
 
 library(rbenchmark)
 benchmark(lower_old(xx), lower(xx),
